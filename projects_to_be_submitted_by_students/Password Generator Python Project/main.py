115a6d809cea4795df08acb1dab8dd4b07744c70
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2025-04-21 01:08:05 +0500

    update

---
+++ projects_to_be_submitted_by_students/Password Generator Python Project/main.py
@@ -0,0 +1,7 @@
+import random 
+
+print("\t \t \t Welcome To Your Password Generator.")
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.!@#$%^&*()<>/1234567890"
+
+number = input("Amount of password to generate:")
