35e2c8fcf7616803aa8d2e0ac88c59b2994cd926
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2025-04-24 18:46:12 +0500

    update

--- projects_to_be_submitted_by_students/BMI Calculator/app.py
+++ projects_to_be_submitted_by_students/BMI Calculator/app.py
@@ -4,3 +4,16 @@ import pandas as pd
 
 st.title("BMI Calculator")
 st.write("Calculate your Body Mass Index (BMI)")
+
+height = st.slider("Enter your height (in cm): ", 100, 250, 175)
+weight = st.slider("Enter your weight (in kg): ", 40, 200, 70)
+
+bmi = weight / (height/100 ** 2)
+
+st.write(f"Your BMI is {bmi:.2f}")
+
+st.write("###  BMI Categories ###")
+st.write("- Under weight: BMI is less than 18.5")
+st.write("- Normal weight: BMI between 18.5 and 24.9 ")
+st.write("- Over weight: BMI between 25 and 29.9 ")
+st.write("- Obesity: BMI 30 or greater ")
